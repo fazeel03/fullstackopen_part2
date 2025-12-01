d4b60a81957582c2f083dbe4bf81744ed8cd8a3f
Fazeel <156305680+fazeel03@users.noreply.github.com>
2025-12-01 19:53:00 +0530

    Add example of using map to extract animal names

---
+++ higher_order_functions/map/example1.js
@@ -0,0 +1,15 @@
+const animals = [
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Caro',       species: 'dog' },
+  { name: 'Hamilton',   species: 'dog' },
+  { name: 'Harold',     species: 'fish' },
+  { name: 'Ursula',     species: 'cat' },
+  { name: 'Jimmy',      species: 'fish' }
+];
+
+
+const names = animals.map(function (animal) {
+  return animal.name;
+});
+
+console.log(names);
