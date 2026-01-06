3eccf1262bfb9e52131f8b796c077d4ea10ef132
Fazeel <156305680+fazeel03@users.noreply.github.com>
2026-01-06 10:53:08 +0530

    Add Course component to display course details

---
+++ ex 2.1 - ex2.5/components/Course.jsx
@@ -0,0 +1,30 @@
+const Course = ({ course }) => {
+  const Header = ({ course }) => <h2>{course.name}</h2>
+
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return <p><strong>total of {total} exercises</strong></p>
+  }
+
+  const Part = ({ part }) => (
+    <p>{part.name} {part.exercises}</p>
+  )
+
+  const Content = ({ parts }) => (
+    <>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  )
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
